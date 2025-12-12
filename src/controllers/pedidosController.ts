9cd7435fefa994f48aa2b1e289de6248e33d5325
AstraAbigail <astrabigail@gmail.com>
2025-12-11 21:48:23 -0300

    filtros ok

--- src/controllers/pedidosController.ts
+++ src/controllers/pedidosController.ts
@@ -13,24 +13,29 @@ import { createPedidoSchema, updatedPedidoSchema } from "../validators/pedidoVal
 class PedidosController {
   static getAllPedidos = async (req: Request, res: Response): Promise<void | Response> => {
     try {
-      const { name, stock, category, minPrice, maxPrice } = req.query
+      const { n_pedido, nombre, identificacion, category, line, model, minCant, maxCant, estado} = req.query
       console.log(req.query)
 
       const filter: any = {}
 
-      if (name) filter.name = new RegExp(String(name), "i")
-      if (stock) filter.stock = Number(stock)
-      if (category) filter.category = new RegExp(String(category), "i")
-      if (minPrice || maxPrice) {
-        filter.price = {}
+      if (n_pedido) filter.n_pedido = Number(n_pedido)
+      if (nombre) filter['cliente.nombre'] = new RegExp(String(nombre), "i")
+      if (identificacion) filter['cliente.identificacion'] = new RegExp(String(identificacion))
+      if (category) filter['productos.category']= new RegExp(String(category), "i")
+      if (line) filter['productos.line'] = new RegExp(String(line), "i")
+      if (model) filter['productos.model'] = new RegExp(String(model), "i")
+      if (minCant || maxCant) {
+        filter['productos.cantidad'] = {}
         // maxPrice -> si tengo precio máximo quiero un objeto con un precio menor
-        if (minPrice) filter.price.$gte = minPrice
+        if (minCant) filter['productos.cantidad'].$gte = Number(minCant)
         // minPrice -> si tengo un precio mínimo quiero un objeto con un precio mas grande.
-        if (maxPrice) filter.price.$lte = maxPrice
+        if (maxCant) filter['productos.cantidad'].$lte = Number(maxCant)
       }
+      if (estado) filter.estado= String(estado)
 
-      const products = await PedidoModel.find(filter)
-      res.json({ success: true, data: products })
+      const pedidos = await PedidoModel.find(filter)
+      console.log("Pedidos encontrados:", pedidos.length)
+      res.json({ success: true, data: pedidos })
     } catch (e) {
       const error = e as Error
       res.status(500).json({ success: false, error: error.message })
@@ -106,69 +111,55 @@ class PedidosController {
     }
   };
 
-  // static addPedido = async (req: Request, res: Response): Promise<void | Response> => {
-  //   try {
-  //     const { body } = req
-
-  //     const { name, description, price, category, stock } = body
-
-  //     if (!name || !description || !price || !category || !stock) {
-  //       return res.status(400).json({ message: "Todos los campos son requeridos" })
-  //     }
-
-  //     const dataToValidate = {
-  //       n_pedido: "",
-  //       nombre: "",
-  //       identificacion: "",
-  //       domicilio: "",
-  //       estado: "",
-  //       cantidad: "",
-  //       category: "",
-  //       line: "",
-  //       model: "",
-  //       marco: "",
-  //       color: "",
-  //       mano: "",
-  //       precio: ""
-        
-  //     }
-
-  //     const validator = createPedidoSchema.safeParse(dataToValidate)
-
-  //     if (!validator.success) {
-  //       return res.status(400).json({ success: false, error: validator.error.flatten().fieldErrors });
-  //     }
-
-  //     const newPedido = new PedidoModel(validator.data)
-
-  //     await newPedido.save()
-  //     res.status(201).json({ success: true, data: newPedido })
-  //   } catch (e) {
-  //     const error = e as Error
-  //     res.status(500).json({ success: false, error: error.message })
-  //   }
-  // }
+  
 
   static updatePedido = async (req: Request, res: Response): Promise<void | Response> => {
     try {
       const { id } = req.params
-      const { body } = req
+      const { indice, productoUpdate } = req.body
 
+
+      console.log(indice, productoUpdate, "DESDE EL BACK ")
       if (!Types.ObjectId.isValid(id)) res.status(400).json({ succes: false, error: "ID Inválido" })
-
-      const validator = updatedPedidoSchema.safeParse(body)
+      // Asegurarse de que el índice sea un número válido
+      if (typeof indice !== 'number' || indice < 0) {
+          return res.status(400).json({succes: false, error: "Indice Inválido"  });
+      }
+      const validator = updatedPedidoSchema.safeParse(productoUpdate)
 
       if (!validator.success) {
         return res.status(400).json({ success: false, error: validator.error.flatten().fieldErrors });
       }
 
-      const updatedPedido = await PedidoModel.findByIdAndUpdate(id, validator.data, { new: true })
+      ////////////////
+      const updateOperation: Record<string, any> = {}
 
-      if (!updatedPedido) {
-        return res.status(404).json({ success: false, error: "Producto no encontrado" })
-      }
+        // 1. Construir la operación $set usando el índice
+        // Ejemplo: si indice=0, el campo es 'productos.0.cantidad'
+        for (const key in  productoUpdate ) {
+            updateOperation[`productos.${indice}.${key}`] =  productoUpdate[key];
+        }
 
-      res.json({ success: true, data: updatedPedido })
+        const pedidoActualizado = await PedidoModel.findByIdAndUpdate(
+            id,
+            { $set: updateOperation },
+            { new: true } 
+        )
+          console.log(pedidoActualizado,"PEDIDO ACTUALIZADO")
+        if (!pedidoActualizado){
+            return res.status(404).json({success: false,error: "Pedido no encontrado." })
+        }
+
+        res.status(200).json({ success: true, data: pedidoActualizado })
+      ////////////////
+
+      // const updatedPedido = await PedidoModel.findByIdAndUpdate(id, validator.data, { new: true })
+
+      // if (!updatedPedido) {
+      //   return res.status(404).json({ success: false, error: "Producto no encontrado" })
+      // }
+
+      // res.json({ success: true, data: updatedPedido })
     } catch (e) {
       const error = e as Error
       res.status(500).json({ success: false, error: error.message })
